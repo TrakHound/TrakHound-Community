f471dd379fe9867bbc264b73ec0fbf992c9f78cb
Patrick Ritchie <Patrick@feenux.com>
2016-02-27 19:19:41 -0500

    Initial Commit

---
+++ Database/TH_SQLite/ConfigurationPage/Button.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TH_SQLite.ConfigurationPage
+{
+    /// <summary>
+    /// Interaction logic for Button.xaml
+    /// </summary>
+    public partial class Button : UserControl
+    {
+        public Button()
+        {
+            InitializeComponent();
+        }
+    }
+}
