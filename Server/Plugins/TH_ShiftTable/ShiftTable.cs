8ec2401c0a2b94c07cbf0a53395f2cc2b43205cb
Patrick Ritchie <Patrick@feenux.com>
2016-03-01 11:30:03 -0500

    Update

--- Server/Plugins/TH_ShiftTable/ShiftTable.cs
+++ Server/Plugins/TH_ShiftTable/ShiftTable.cs
@@ -39,12 +39,12 @@ namespace TH_ShiftTable
 
                 config = configuration;
 
-                if (UseDatabases)
-                {
+                //if (UseDatabases)
+                //{
                     CreateShiftSegmentsTable(sc.shifts);
                     CreateTable();
                     GetTableColumns();
-                }
+                //}
 
                 AddGeneratedEventColumns();
 
@@ -158,8 +158,6 @@ namespace TH_ShiftTable
 
         void CreateShiftSegmentsTable(List<Shift> shifts)
         {
-            Table.Truncate(config.Databases_Server, TableNames.ShiftSegments);
-
             List<ColumnDefinition> columns = new List<ColumnDefinition>()
             {
                 new ColumnDefinition("SHIFT", DataType.LargeText),
@@ -172,7 +170,7 @@ namespace TH_ShiftTable
                 new ColumnDefinition("TYPE", DataType.LargeText)
             };
 
-            Table.Create(config.Databases_Server, TableNames.ShiftSegments, columns.ToArray(), null);
+            Table.Replace(config.Databases_Server, TableNames.ShiftSegments, columns.ToArray(), null);
 
             if (shifts != null)
             {
@@ -208,7 +206,6 @@ namespace TH_ShiftTable
                 Row.Insert(config.Databases_Server, TableNames.ShiftSegments, insertColumns.ToArray(), rowValues, null, true);
             }
         }
-
 
 
         public string TableName = TableNames.Shifts;
