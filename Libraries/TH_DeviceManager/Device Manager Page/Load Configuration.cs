2819ffa532631989a354b6d440caeba543cd3958
Patrick Ritchie <Patrick@feenux.com>
2016-03-02 22:54:39 -0500

    Update

---
+++ Libraries/TH_DeviceManager/Device Manager Page/Load Configuration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TH_Plugins_Server;
+
+namespace TH_DeviceManager
+{
+    public partial class DeviceManagerPage
+    {
+        void LoadConfiguration()
+        {
+            if (ConfigurationTable != null)
+            {
+                if (ConfigurationPages != null)
+                {
+                    foreach (ConfigurationPage page in ConfigurationPages)
+                    {
+                        page.LoadConfiguration(ConfigurationTable);
+                    }
+                }
+            }
+        }
+
+    }
+}
