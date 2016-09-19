270ce44d4432952167b906cb81a035b2ba636b1f
Patrick Ritchie <Patrick@Feenux.com>
2016-09-19 13:55:58 -0400

    Added more icons to App.xaml to use throughout TrakHound-Dashboard for resources

--- Version.cs
+++ Version.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 
-[assembly: AssemblyVersion("1.3.4.*")]
-[assembly: AssemblyFileVersion("1.3.4.*")]
+[assembly: AssemblyVersion("1.3.7.*")]
+[assembly: AssemblyFileVersion("1.3.7.*")]
