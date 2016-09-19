d37450f18e1dcd464a6fac0593b84ea62fbf0337
Patrick Ritchie <Patrick@Feenux.com>
2016-09-19 13:23:57 -0400

    Cleaned up using statements

--- TrakHound-Dashboard/Pages/Dashboard/DeviceStatusTimes/Plugin.cs
+++ TrakHound-Dashboard/Pages/Dashboard/DeviceStatusTimes/Plugin.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿// Copyright (c) 2016 Feenux LLC, All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 using TrakHound;
-using TrakHound.Tools;
-using TrakHound.Logging;
-using TrakHound.Configurations;
 using TrakHound.Plugins;
 using TrakHound.Plugins.Client;
+using TrakHound.Tools;
 
 namespace TrakHound_Dashboard.Pages.Dashboard.DeviceStatusTimes
 {
