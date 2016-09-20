ee6286027dec0bb08a93c835f1a7471e3000678e
Patrick Ritchie <Patrick@Feenux.com>
2016-09-19 23:28:07 -0400

    Added the DeviceDescription class for lightweight client device lists that contain only the device description and any other simple data

---
+++ TrakHound.NET/Configurations/DeviceDescription.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2016 Feenux LLC, All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using TrakHound.API;
+
+namespace TrakHound.Configurations
+{
+    public class DeviceDescription
+    {
+        public DeviceDescription() { }
+
+        public DeviceDescription(Data.DeviceInfo deviceInfo)
+        {
+            UniqueId = deviceInfo.UniqueId;
+            Description = deviceInfo.Description;
+            Agent = deviceInfo.Agent;
+        }
+
+        public DeviceDescription(DeviceConfiguration deviceConfig)
+        {
+            UniqueId = deviceConfig.UniqueId;
+            Description = deviceConfig.Description;
+            Agent = deviceConfig.Agent;
+        }
+
+
+        public string UniqueId { get; set; }
+
+        public Data.DescriptionInfo Description { get; set; }
+
+        public Data.AgentInfo Agent { get; set; }
+    }
+}
