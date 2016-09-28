7b15e23fff385077fe355069b163418682e41ff0
Patrick Ritchie <Patrick@Feenux.com>
2016-09-28 00:27:57 -0400

    Added the UppercaseFirst converter class

---
+++ TrakHound-UI/Converters/UppercaseFirst.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) 2016 Feenux LLC, All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+using TrakHound.Tools;
+
+namespace TrakHound_UI
+{
+    public abstract class BaseConverter : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+
+    [ValueConversion(typeof(object), typeof(string))]
+    public class UppercaseConverter : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                string val = value.ToString();
+
+                return String_Functions.UppercaseFirst(val);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
