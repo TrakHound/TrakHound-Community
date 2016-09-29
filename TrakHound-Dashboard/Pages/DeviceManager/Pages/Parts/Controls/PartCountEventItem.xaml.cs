332e376e540f13a07d55804f2f6d5d952282a29b
Patrick Ritchie <Patrick@Feenux.com>
2016-09-29 00:08:42 -0400

    Updated DeviceManger.Pages.Parts to use new Part Count Configuration

---
+++ TrakHound-Dashboard/Pages/DeviceManager/Pages/Parts/Controls/PartCountEventItem.xaml.cs
@@ -0,0 +1,218 @@
+﻿// Copyright (c) 2016 Feenux LLC, All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+using TrakHound.Tools;
+
+namespace TrakHound_Dashboard.Pages.DeviceManager.Pages.Parts.Controls
+{
+    /// <summary>
+    /// Interaction logic for ProductionTypeItem.xaml
+    /// </summary>
+    public partial class PartCountEventItem : UserControl
+    {
+        public PartCountEventItem()
+        {
+            InitializeComponent();
+            root.DataContext = this;
+
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        public delegate void SettingChanged_Handler();
+        public event SettingChanged_Handler SettingChanged;
+
+        public delegate void RemoveClicked_Handler(PartCountEventItem item);
+        public event RemoveClicked_Handler RemoveClicked;
+
+        #region "Dependency Properties"
+
+        public Page ParentPage
+        {
+            get { return (Page)GetValue(ParentPageProperty); }
+            set { SetValue(ParentPageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ParentPageProperty =
+            DependencyProperty.Register("ParentPage", typeof(Page), typeof(PartCountEventItem), new PropertyMetadata(null));
+
+
+        public string EventName
+        {
+            get { return (string)GetValue(EventNameProperty); }
+            set { SetValue(EventNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty EventNameProperty =
+            DependencyProperty.Register("EventName", typeof(string), typeof(PartCountEventItem), new PropertyMetadata(null));
+
+
+        public string EventValue
+        {
+            get { return (string)GetValue(EventValueProperty); }
+            set { SetValue(EventValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty EventValueProperty =
+            DependencyProperty.Register("EventValue", typeof(string), typeof(PartCountEventItem), new PropertyMetadata(null));
+
+
+        public string CaptureItemLink
+        {
+            get { return (string)GetValue(CaptureItemLinkProperty); }
+            set { SetValue(CaptureItemLinkProperty, value); }
+        }
+
+        public static readonly DependencyProperty CaptureItemLinkProperty =
+            DependencyProperty.Register("CaptureItemLink", typeof(string), typeof(PartCountEventItem), new PropertyMetadata(null));
+
+
+        public string CalculationType
+        {
+            get { return (string)GetValue(CalculationTypeProperty); }
+            set { SetValue(CalculationTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty CalculationTypeProperty =
+            DependencyProperty.Register("CalculationType", typeof(string), typeof(PartCountEventItem), new PropertyMetadata("Total"));
+
+
+        #endregion
+
+
+        private void COMBO_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = (ComboBox)sender;
+
+            if (combo.IsKeyboardFocusWithin || combo.IsMouseCaptured)
+            {
+                SettingChanged?.Invoke();
+            }
+        }
+
+        private void EventName_COMBO_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox combo = (ComboBox)sender;
+
+            string val = (string)combo.SelectedValue;
+
+            GetGeneratedEventValues(val);
+            GetCaptureItems(val);
+
+            if (combo.IsKeyboardFocusWithin || combo.IsMouseCaptured)
+            {
+                SettingChanged?.Invoke();
+            }
+        }
+
+        private void Remove_Clicked(TrakHound_UI.Button bt)
+        {
+            RemoveClicked?.Invoke(this);
+        }
+
+        #region "Generated Event Values"
+
+        private ObservableCollection<object> _generatedEventValues;
+        public ObservableCollection<object> GeneratedEventValues
+        {
+            get
+            {
+                if (_generatedEventValues == null)
+                    _generatedEventValues = new ObservableCollection<object>();
+                return _generatedEventValues;
+            }
+
+            set
+            {
+                _generatedEventValues = value;
+            }
+        }
+
+        private void GetGeneratedEventValues(string Id)
+        {
+            GeneratedEventValues.Clear();
+
+            if (ParentPage != null && ParentPage.GeneratedEventItems != null)
+            {
+                var e = ParentPage.GeneratedEventItems.ToList().Find(x => x.Id == Id);
+                if (e != null)
+                {
+                    // Add each Value
+                    foreach (var value in e.Event.values)
+                    {
+                        if (value.result != null) GeneratedEventValues.Add(value.result.value);
+                    }
+
+                    // Add Default Value
+                    if (e.Event.Default != null)
+                    {
+                        if (e.Event.Default != null) GeneratedEventValues.Add(e.Event.Default.value);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+        #region "Capture Items"
+
+        public class CaptureItem
+        {
+            public CaptureItem(GeneratedEvents.Page.CaptureItem item)
+            {
+                Id = item.name;
+                Name = String_Functions.UppercaseFirst(item.name.Replace('_', ' '));
+            }
+
+            public string Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        private ObservableCollection<CaptureItem> _generatedEventCaptureItems;
+        public ObservableCollection<CaptureItem> GeneratedEventCaptureItems
+        {
+            get
+            {
+                if (_generatedEventCaptureItems == null)
+                    _generatedEventCaptureItems = new ObservableCollection<CaptureItem>();
+                return _generatedEventCaptureItems;
+            }
+
+            set
+            {
+                _generatedEventCaptureItems = value;
+            }
+        }
+
+        private void GetCaptureItems(string Id)
+        {
+            GeneratedEventCaptureItems.Clear();
+
+            if (ParentPage != null && ParentPage.GeneratedEventItems != null)
+            {
+                var e = ParentPage.GeneratedEventItems.ToList().Find(x => x.Id == Id);
+                if (e != null)
+                {
+                    // Add each Value
+                    foreach (var item in e.Event.captureItems)
+                    {
+                        var i = new CaptureItem(item);
+                        GeneratedEventCaptureItems.Add(i);
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
--- TrakHound-Dashboard/Pages/DeviceManager/Pages/Parts/Page.xaml.cs
+++ TrakHound-Dashboard/Pages/DeviceManager/Pages/Parts/Page.xaml.cs
@@ -27,9 +27,6 @@ namespace TrakHound_Dashboard.Pages.DeviceManager.Pages.Parts
         {
             InitializeComponent();
             DataContext = this;
-
-            CalculationTypes.Add(CalculationType.Incremental.ToString());
-            CalculationTypes.Add(CalculationType.Total.ToString());
         }
 
         public string Title { get { return "Parts"; } }
@@ -48,42 +45,34 @@ namespace TrakHound_Dashboard.Pages.DeviceManager.Pages.Parts
 
         public void LoadConfiguration(DataTable dt)
         {
-            configurationTable = dt;
-
             LoadGeneratedEventItems(dt);
-
-            ClearData();
-            LoadData(dt);
-        }
-
-        void ClearData()
-        {
-            SelectedPartsEventName = null;
-            SelectedProducedEventValue = null;
-            SelectedPartCountLink = null;
-        }
-
-        void LoadData(DataTable dt)
-        {
-            LoadPartsEventName(dt);
-            LoadProducedEventValueName(dt);
-            LoadPartCountLink(dt);
-            LoadCalculationType(dt);      
+            LoadPartCountEventItems(dt);
         }
 
         public void SaveConfiguration(DataTable dt)
-        { 
-            SavePartsEventName(dt);
-            SaveProducedEventValueName(dt);
-            SavePartCountLink(dt);
-            SaveCalculationType(dt);
-        }
+        {
+            DataTable_Functions.TrakHound.DeleteRows(prefix + "*", "address", dt);
 
-        DataTable configurationTable;
+            int i = 0;
+
+            foreach (var item in PartCountItems)
+            {
+                string eventPrefix = prefix + "/Event||" + i.ToString("00");
+
+                DataTable_Functions.UpdateTableValue(dt, "address", eventPrefix, "attributes", "id||" + i.ToString("00") + ";");
+                DataTable_Functions.UpdateTableValue(dt, "address", eventPrefix + "/EventName", "value", item.EventName);
+                DataTable_Functions.UpdateTableValue(dt, "address", eventPrefix + "/EventValue", "value", item.EventValue);
+                DataTable_Functions.UpdateTableValue(dt, "address", eventPrefix + "/CaptureItemLink", "value", item.CaptureItemLink);
+                DataTable_Functions.UpdateTableValue(dt, "address", eventPrefix + "/CalculationType", "value", item.CalculationType);
+
+                i++;
+            }
+        }
 
         const string prefix = "/Parts/";
 
-        ObservableCollection<GeneratedEventItem> _generatedEventItems;
+
+        private ObservableCollection<GeneratedEventItem> _generatedEventItems;
         public ObservableCollection<GeneratedEventItem> GeneratedEventItems
         {
             get
@@ -99,25 +88,9 @@ namespace TrakHound_Dashboard.Pages.DeviceManager.Pages.Parts
             }
         }
 
-        ObservableCollection<string> _calculationTypes;
-        public ObservableCollection<string> CalculationTypes
-        {
-            get
-            {
-                if (_calculationTypes == null)
-                    _calculationTypes = new ObservableCollection<string>();
-                return _calculationTypes;
-            }
-
-            set
-            {
-                _calculationTypes = value;
-            }
-        }
-
         public class GeneratedEventItem
         {
-            public GeneratedEventItem(TrakHound_Dashboard.Pages.DeviceManager.Pages.GeneratedEvents.Page.Event e)
+            public GeneratedEventItem(GeneratedEvents.Page.Event e)
             {
                 Id = e.name;
                 Name = String_Functions.UppercaseFirst(e.name.Replace('_', ' ').ToLower());
@@ -127,360 +100,99 @@ namespace TrakHound_Dashboard.Pages.DeviceManager.Pages.Parts
             public string Id { get; set; }
             public string Name { get; set; }
 
-            public TrakHound_Dashboard.Pages.DeviceManager.Pages.GeneratedEvents.Page.Event Event { get; set; }
+            public GeneratedEvents.Page.Event Event { get; set; }
         }
 
         private void LoadGeneratedEventItems(DataTable dt)
         {
             GeneratedEventItems.Clear();
 
-            var events = TrakHound_Dashboard.Pages.DeviceManager.Pages.GeneratedEvents.Page.GetGeneratedEvents(dt);
+            var events = GeneratedEvents.Page.GetGeneratedEvents(dt);
             foreach (var e in events)
             {
                 GeneratedEventItems.Add(new GeneratedEventItem(e));
             }
         }
+        
 
-
-        #region "Generated Events"
-
-        ObservableCollection<object> _generatedEventValues;
-        public ObservableCollection<object> GeneratedEventValues
+        private ObservableCollection<Controls.PartCountEventItem> _partCountItems;
+        public ObservableCollection<Controls.PartCountEventItem> PartCountItems
         {
             get
             {
-                if (_generatedEventValues == null)
-                    _generatedEventValues = new ObservableCollection<object>();
-                return _generatedEventValues;
+                if (_partCountItems == null)
+                    _partCountItems = new ObservableCollection<Controls.PartCountEventItem>();
+                return _partCountItems;
             }
 
             set
             {
-                _generatedEventValues = value;
+                _partCountItems = value;
             }
         }
 
-        void GetGeneratedEventValues(string Id)
+        private void LoadPartCountEventItems(DataTable dt)
         {
-            GeneratedEventValues.Clear();
+            PartCountItems.Clear();
 
-            if (GeneratedEventItems != null)
+            string address = "/Parts/";
+            string filter = "address LIKE '" + address + "*'";
+
+            var rows = DataTable_Functions.GetRows(dt, filter);
+
+            foreach (DataRow row in rows)
             {
-                var e = GeneratedEventItems.ToList().Find(x => x.Id == Id);
-                if (e != null)
+                string lastNode = DataTable_Functions.TrakHound.GetLastNode(row);
+                if (lastNode == "Event")
                 {
-                    // Add each Value
-                    foreach (var value in e.Event.values)
+                    var item = new Controls.PartCountEventItem();
+                    item.ParentPage = this;
+                    item.RemoveClicked += Item_RemoveClicked;
+                    item.SettingChanged += Item_SettingChanged;
+
+                    filter = "address LIKE '" + row["address"].ToString() + "/*'";
+                    var eventRows = DataTable_Functions.GetRows(dt, filter);
+                    foreach (DataRow eventRow in eventRows)
                     {
-                        if (value.result != null) GeneratedEventValues.Add(value.result.value);
+                        lastNode = DataTable_Functions.TrakHound.GetLastNode(eventRow);
+
+                        switch (lastNode)
+                        {
+                            case "EventName": item.EventName = DataTable_Functions.GetRowValue("value", eventRow); break;
+                            case "EventValue": item.EventValue = DataTable_Functions.GetRowValue("value", eventRow); break;
+                            case "CaptureItemLink": item.CaptureItemLink = DataTable_Functions.GetRowValue("value", eventRow); break;
+                            case "CalculationType": item.CalculationType = DataTable_Functions.GetRowValue("value", eventRow); break;
+                        }
                     }
 
-                    // Add Default Value
-                    if (e.Event.Default != null)
-                    {
-                        if (e.Event.Default != null) GeneratedEventValues.Add(e.Event.Default.value);
-                    }
+                    PartCountItems.Add(item);
                 }
             }
         }
 
-        public class CaptureItem
+        private void Add_Clicked(TrakHound_UI.Button bt)
         {
-            public CaptureItem(TrakHound_Dashboard.Pages.DeviceManager.Pages.GeneratedEvents.Page.CaptureItem item)
-            {
-                Id = item.name;
-                Name = String_Functions.UppercaseFirst(item.name.Replace('_', ' '));
-            }
-
-            public string Id { get; set; }
-            public string Name { get; set; }
+            var item = new Controls.PartCountEventItem();
+            item.ParentPage = this;
+            item.RemoveClicked += Item_RemoveClicked;
+            item.SettingChanged += Item_SettingChanged;
+            PartCountItems.Add(item);
         }
 
-        ObservableCollection<CaptureItem> _generatedEventCaptureItems;
-        public ObservableCollection<CaptureItem> GeneratedEventCaptureItems
+        private void Item_SettingChanged()
         {
-            get
-            {
-                if (_generatedEventCaptureItems == null)
-                    _generatedEventCaptureItems = new ObservableCollection<CaptureItem>();
-                return _generatedEventCaptureItems;
-            }
+            SettingChanged?.Invoke(null, null, null);
+        }
 
-            set
+        private void Item_RemoveClicked(Controls.PartCountEventItem item)
+        {
+            int i = PartCountItems.ToList().FindIndex(o => o.Id == item.Id);
+            if (i >= 0)
             {
-                _generatedEventCaptureItems = value;
+                PartCountItems.RemoveAt(i);
+                SettingChanged?.Invoke(null, null, null);
             }
         }
 
-        void GetGeneratedEventCaptureItems(string Id)
-        {
-            GeneratedEventCaptureItems.Clear();
-
-            if (GeneratedEventItems != null)
-            {
-                var e = GeneratedEventItems.ToList().Find(x => x.Id == Id);
-                if (e != null)
-                {
-                    // Add each Value
-                    foreach (var item in e.Event.captureItems)
-                    {
-                        var i = new CaptureItem(item);
-                        GeneratedEventCaptureItems.Add(i);
-                    }
-                }
-            }
-        }
-
-        #endregion
-
-        #region "Cycle Event Name"
-
-        public object SelectedPartsEventName
-        {
-            get { return (object)GetValue(SelectedPartsEventNameProperty); }
-            set { SetValue(SelectedPartsEventNameProperty, value); }
-        }
-
-        public static readonly DependencyProperty SelectedPartsEventNameProperty =
-            DependencyProperty.Register("SelectedPartsEventName", typeof(object), typeof(Page), new PropertyMetadata(null));
-
-        private void PartsEventName_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBox cmbox = (ComboBox)sender;
-            if (cmbox.SelectedItem != null)
-            {
-                var item = (GeneratedEventItem)cmbox.SelectedItem;
-                GetGeneratedEventValues(item.Id);
-                GetGeneratedEventCaptureItems(item.Id);
-            }
-
-            if (cmbox.IsKeyboardFocused || cmbox.IsMouseCaptured)
-            {
-                if (SettingChanged != null) SettingChanged("Parts Event Name", null, null);
-            }
-        }
-
-        void LoadPartsEventName(DataTable dt)
-        {
-            string query = "Address = '" + prefix + "PartsEventName'";
-
-            var rows = dt.Select(query);
-            if (rows != null)
-            {
-                foreach (var row in rows)
-                {
-                    string val = DataTable_Functions.GetRowValue("Value", row);
-                    if (val != null) SelectedPartsEventName = val;
-                }
-            }
-        }
-
-        void SavePartsEventName(DataTable dt)
-        {
-            string val = "";
-            if (SelectedPartsEventName != null) val = SelectedPartsEventName.ToString();
-
-            if (val != null) DataTable_Functions.UpdateTableValue(dt, "address", prefix + "PartsEventName", "value", val.Replace(' ', '_').ToLower());
-        }
-
-        #endregion
-
-        #region "Parts Produced Event Value"
-
-        public object SelectedProducedEventValue
-        {
-            get { return (object)GetValue(SelectedProducedEventValueProperty); }
-            set { SetValue(SelectedProducedEventValueProperty, value); }
-        }
-
-        public static readonly DependencyProperty SelectedProducedEventValueProperty =
-            DependencyProperty.Register("SelectedProducedEventValue", typeof(object), typeof(Page), new PropertyMetadata(null));
-
-
-        private void producedValue_COMBO_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBox cmbox = (ComboBox)sender;
-            if (cmbox.IsKeyboardFocused || cmbox.IsMouseCaptured) if (SettingChanged != null) SettingChanged("Parts Produced Event Value", null, null);
-        }
-
-        void LoadProducedEventValueName(DataTable dt)
-        {
-            string query = "Address = '" + prefix + "PartsEventValue'";
-
-            var rows = dt.Select(query);
-            if (rows != null)
-            {
-                foreach (var row in rows)
-                {
-                    string val = DataTable_Functions.GetRowValue("Value", row);
-                    if (val != null) SelectedProducedEventValue = String_Functions.UppercaseFirst(val.Replace('_', ' '));
-                }
-            }
-        }
-
-        void SaveProducedEventValueName(DataTable dt)
-        {
-            string val = "";
-            if (SelectedProducedEventValue != null) val = SelectedProducedEventValue.ToString();
-
-            if (val != null) DataTable_Functions.UpdateTableValue(dt, "address", prefix + "PartsEventValue", "value", val.Replace(' ', '_').ToLower());
-        }
-
-        #endregion
-
-        #region "Part Count Link"
-
-        public object SelectedPartCountLink
-        {
-            get { return (object)GetValue(SelectedPartCountLinkProperty); }
-            set { SetValue(SelectedPartCountLinkProperty, value); }
-        }
-
-        public static readonly DependencyProperty SelectedPartCountLinkProperty =
-            DependencyProperty.Register("SelectedPartCountLink", typeof(object), typeof(Page), new PropertyMetadata(null));
-
-        private void partCountLink_COMBO_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBox cmbox = (ComboBox)sender;
-            if (cmbox.IsKeyboardFocused || cmbox.IsMouseCaptured) if (SettingChanged != null) SettingChanged("Part Count Link", null, null);
-        }
-
-        void LoadPartCountLink(DataTable dt)
-        {
-            string query = "Address = '" + prefix + "PartsCaptureItemLink'";
-
-            var rows = dt.Select(query);
-            if (rows != null)
-            {
-                foreach (var row in rows)
-                {
-                    string val = DataTable_Functions.GetRowValue("Value", row);
-                    if (val != null) SelectedPartCountLink = val;
-                }
-            }
-        }
-
-        void SavePartCountLink(DataTable dt)
-        {
-            string val = "";
-            if (SelectedPartCountLink != null) val = SelectedPartCountLink.ToString();
-
-            if (val != null) DataTable_Functions.UpdateTableValue(dt, "address", prefix + "PartsCaptureItemLink", "value", val.Replace(' ', '_').ToLower());
-        }
-
-        #endregion
-
-        #region "Calculation Type"
-
-        public string SelectedCalculationType
-        {
-            get { return (string)GetValue(SelectedCalculationTypeProperty); }
-            set { SetValue(SelectedCalculationTypeProperty, value); }
-        }
-
-        public static readonly DependencyProperty SelectedCalculationTypeProperty =
-            DependencyProperty.Register("SelectedCalculationType", typeof(string), typeof(Page), new PropertyMetadata("Interval"));
-
-
-
-        private void CalculationType_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ComboBox cmbox = (ComboBox)sender;
-            if (cmbox.IsKeyboardFocused || cmbox.IsMouseCaptured) if (SettingChanged != null) SettingChanged("Calculation Type Changed", null, null);
-        }
-
-        void LoadCalculationType(DataTable dt)
-        {
-            string query = "Address = '" + prefix + "CalculationType'";
-
-            var rows = dt.Select(query);
-            if (rows != null)
-            {
-                foreach (var row in rows)
-                {
-                    string val = DataTable_Functions.GetRowValue("Value", row);
-                    if (val != null) SelectedCalculationType = val;
-                }
-            }
-        }
-
-        void SaveCalculationType(DataTable dt)
-        {
-            string val = "";
-            if (SelectedPartCountLink != null) val = SelectedCalculationType.ToString();
-
-            if (val != null) DataTable_Functions.UpdateTableValue(dt, "address", prefix + "CalculationType", "value", val);
-        }
-
-        #endregion
-
-        private void Help_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (sender.GetType() == typeof(Rectangle))
-            {
-                Rectangle rect = (Rectangle)sender;
-
-                if (rect.ToolTip != null)
-                {
-                    if (rect.ToolTip.GetType() == typeof(ToolTip))
-                    {
-                        ToolTip tt = (ToolTip)rect.ToolTip;
-                        tt.IsOpen = true;
-                    }
-                }
-            }
-        }
-
-        private void Help_MouseEnter(object sender, MouseEventArgs e)
-        {
-            if (sender.GetType() == typeof(Rectangle))
-            {
-                Rectangle rect = (Rectangle)sender;
-
-                if (rect.ToolTip != null)
-                {
-                    if (rect.ToolTip.GetType() == typeof(ToolTip))
-                    {
-                        ToolTip tt = (ToolTip)rect.ToolTip;
-                        tt.IsOpen = true;
-                    }
-                }
-            }
-        }
-
-        private void Help_MouseLeave(object sender, MouseEventArgs e)
-        {
-            if (sender.GetType() == typeof(Rectangle))
-            {
-                Rectangle rect = (Rectangle)sender;
-
-                if (rect.ToolTip != null)
-                {
-                    if (rect.ToolTip.GetType() == typeof(ToolTip))
-                    {
-                        ToolTip tt = (ToolTip)rect.ToolTip;
-                        tt.IsOpen = false;
-                    }
-                }
-            }
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-    }
-
-    public enum CalculationType
-    {
-        /// <summary>
-        /// Incrementally increased as each event is added to the total
-        /// </summary>
-        Incremental,
-
-        /// <summary>
-        /// Event carries total value
-        /// </summary>
-        Total,
     }
 }
