6123730ac5f9abd28bb19c55df7ff8183f8b7151
Patrick Ritchie <Patrick@Feenux.com>
2016-10-05 08:12:03 -0400

    Added GetChildOfType method to find a control in a visualtree

---
+++ TrakHound-UI/Extensions/VisualTree_Functions.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2016 Feenux LLC, All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System.Windows;
+using System.Windows.Media;
+
+namespace TrakHound_UI.Extensions
+{
+    public static class VisualTree_Functions
+    {
+        public static T GetChildOfType<T>(this DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) return null;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+
+                var result = (child as T) ?? GetChildOfType<T>(child);
+                if (result != null) return result;
+            }
+            return null;
+        }
+    }
+}
