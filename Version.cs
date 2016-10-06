31af46184ec4ec1e72123e501564bfe42e24112e
Patrick Ritchie <Patrick@Feenux.com>
2016-10-06 09:59:59 -0400

    Update v1.4.2

--- Version.cs
+++ Version.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 
-[assembly: AssemblyVersion("1.4.0.*")]
-[assembly: AssemblyFileVersion("1.4.0.*")]
+[assembly: AssemblyVersion("1.4.2.*")]
+[assembly: AssemblyFileVersion("1.4.2.*")]
