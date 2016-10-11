a19710c7b7ac16baf54e017d0081f221b24a009c
Patrick Ritchie <Patrick@Feenux.com>
2016-10-11 07:15:25 -0400

    Added Override Info class

---
+++ TrakHound.NET/API/Data/OverrideInfo.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2016 Feenux LLC, All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using Newtonsoft.Json;
+using System;
+
+namespace TrakHound.API
+{
+    public static partial class Data
+    {
+        public class OverrideInfo
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("type")]
+            public string Type { get; set; }
+
+            [JsonProperty("value")]
+            public double Value { get; set; }
+
+            [JsonProperty("timestamp")]
+            public DateTime Timestamp { get; set; }
+        }
+    }
+}
