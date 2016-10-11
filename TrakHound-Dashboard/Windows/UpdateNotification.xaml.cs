587535b3756ea82d0884fec8ec2a5342e5cf6e85
Patrick Ritchie <Patrick@Feenux.com>
2016-10-11 07:09:42 -0400

    Added Update Notification Window to show information on version

---
+++ TrakHound-Dashboard/Windows/UpdateNotification.xaml.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) 2016 Feenux LLC, All Rights Reserved.
+
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System.Windows;
+
+namespace TrakHound_Dashboard
+{
+    /// <summary>
+    /// Interaction logic for UpdateNotification.xaml
+    /// </summary>
+    public partial class UpdateNotification : Window
+    {
+        public UpdateNotification()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindow mw;
+
+        private void DeviceManager_Clicked(TrakHound_UI.Button bt)
+        {
+            mw.DeviceManager_DeviceList_Open();
+            Close();
+        }
+
+        private void Close_Clicked(TrakHound_UI.Button bt)
+        {
+            Close();
+        }
+    }
+}
