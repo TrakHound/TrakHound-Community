5f1e68b9c2acd283878ca7c29bf0ea58bf936f39
Patrick Ritchie <Patrick@Feenux.com>
2016-11-16 01:32:49 -0500

    Updated Project settings and resources

--- TrakHound-Dashboard/Properties/Resources.Designer.cs
+++ TrakHound-Dashboard/Properties/Resources.Designer.cs
@@ -803,6 +803,16 @@ namespace TrakHound_Dashboard.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Send_01 {
+            get {
+                object obj = ResourceManager.GetObject("Send_01", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Share_01 {
             get {
                 object obj = ResourceManager.GetObject("Share_01", resourceCulture);
