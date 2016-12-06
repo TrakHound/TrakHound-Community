b45a9daac823c2e9c35c1638ec5325342af8a76b
Patrick Ritchie <Patrick@TrakHound.com>
2016-12-05 20:48:22 -0500

    Changed default event for Parts to Cycle_Execution to imporve accuracy of part count

--- TrakHound.NET/Configurations/AutoGenerate/Parts.cs
+++ TrakHound.NET/Configurations/AutoGenerate/Parts.cs
@@ -3,11 +3,9 @@
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
 
-using MTConnect;
 using MTConnect.Application.Components;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 
 namespace TrakHound.Configurations.AutoGenerate
 {
@@ -15,48 +13,23 @@ namespace TrakHound.Configurations.AutoGenerate
     {
         public static void Add(DataTable table, List<DataItem> dataItems)
         {
-            //int i = 1;
             int e = 0;
+
+            //DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00"), null, "id||" + e.ToString("00") + ";");
+
+            //DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/EventName", "device_status", null);
+            //DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/EventValue", "Idle", null);
+            //DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/PreviousEventValue", "Active", null);
+            //DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/ValueType", "static_increment", null);
+            //DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/StaticIncrementValue", 1, null);
 
             DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00"), null, "id||" + e.ToString("00") + ";");
 
-            DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/EventName", "device_status", null);
-            DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/EventValue", "Idle", null);
-            DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/PreviousEventValue", "Active", null);
+            DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/EventName", "cycle_execution", null);
+            DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/EventValue", "Paused", null);
+            DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/PreviousEventValue", "Started", null);
             DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/ValueType", "static_increment", null);
             DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/StaticIncrementValue", 1, null);
-
-
-
-            //int i = 1;
-            //int e = 0;
-
-            //var partCountItems = dataItems.FindAll(x => x.Category == DataItemCategory.EVENT && x.Type == "PART_COUNT");
-            //if (partCountItems.Any())
-            //{
-            //    foreach (var partCountItem in partCountItems)
-            //    {
-            //        DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00"), null, "id||" + e.ToString("00") + ";");
-
-            //        DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/EventName", "parts_count_" + i.ToString(), null);
-            //        DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/EventValue", "Parts Produced", null);
-            //        DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/CaptureItemLink", "part_count", null);
-            //        DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/CalculationType", "Total", null);
-
-            //        i++;
-            //        e++;
-            //    }
-            //}
-            //else
-            //{
-            //    DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00"), null, "id||" + e.ToString("00") + ";");
-
-            //    DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/EventName", "device_status", null);
-            //    DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/EventValue", "Idle", null);
-            //    DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/PreviousEventValue", "Active", null);
-            //    DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/ValueType", "static_increment", null);
-            //    DeviceConfiguration.EditTable(table, "/Parts/Event||" + e.ToString("00") + "/StaticIncrementValue", 1, null);
-            //}
         }
     }
 }
